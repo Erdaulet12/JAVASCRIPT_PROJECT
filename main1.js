1200abea10257e8fcd024c43582b8ca06d5768d8
Erdaulet <erdauletakilzhan83@gmail.com>
2024-05-25 23:09:04 +0500

    Рефакторинг

--- main1.js
+++ main1.js
@@ -1,56 +1,44 @@
 const signInBtn = document.querySelector(".signin-btn");
 const signUpBtn = document.querySelector(".signup-btn");
 const formBox = document.querySelector(".form-box");
-const signinForm = document.querySelector(".form__input");
-const signupForm = document.querySelector(".form__input");
-const logininInput = document.querySelector('.form__input[name="login"]');
-const passwordinInput = document.querySelector('.form__input[name="password"]');
-const loginupInput = document.querySelector('.form__input[name="login"]');
-const emailInput = document.querySelector('.form__input[name="email"]');
-const passwordupInput = document.querySelector('.form__input[name="password"]');
-const confirmPasswordInput = document.querySelector(
-  '.form__input[name="confirm_password"]'
-);
 const body = document.body;
+// const emailInput = document.querySelector(".form__input").value;
+// const confirmPasswordInput = document.querySelector(".form__input").value;
 
 signUpBtn.addEventListener("click", function () {
   formBox.classList.add("active");
   body.classList.add("active");
 });
+
 signInBtn.addEventListener("click", function () {
   formBox.classList.remove("active");
   body.classList.remove("active");
 });
 
-signInBtn.addEventListener("DOMContentLoaded", function () {
-  signinForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+function validateLoginForm() {
+  const loginInput = document.querySelector(".login_input").value;
+  const passwordInput = document.querySelector(".password_input").value;
+  if (
+    loginInput.trim() === "" ||
+    passwordInput.trim() === ""
+    // emailInput.trim() === "" ||
+    // confirmPasswordInput.trim() === ""
+  ) {
+    alert("Не все поля заполнены!");
+    return false;
+  }
 
-    if (
-      logininInput.value.trim() === "" ||
-      passwordinInput.value.trim() === ""
-    ) {
-      alert("Не все поля заполнены");
-      return;
-    }
-  });
+  // if (passwordInput !== confirmPasswordInput) {
+  //   alert("Пароли не совпадают!");
+  //   return false;
+  // }
 
-  signupForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  return true;
+}
 
-    if (
-      loginupInput.value.trim() === "" ||
-      emailInput.value.trim() === "" ||
-      passwordupInput.value.trim() === "" ||
-      confirmPasswordInput.value.trim() === ""
-    ) {
-      alert("Пожалуйста, заполните все поля.");
-      return;
-    }
-
-    if (passwordupInput.value !== confirmPasswordInput.value) {
-      alert("Пароли не совпадают.");
-      return;
-    }
-  });
+formBox.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (validateLoginForm(true)) {
+    window.location.href = "page 2/index2.html";
+  }
 });
